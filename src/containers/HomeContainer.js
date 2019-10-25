8572eda07a39f22deebc25f460e8929b4c41ea14
github username <github email address>
2019-10-25 12:02:17 -0500

    fixed home page

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -5,31 +5,40 @@ import UserStats from '../components/UserStats'
 class HomeContainer extends Component {
     state = {  }
 
-    componentDidUpdate() {
+    componentDidMount() {
         if(!!this.props.currentUser && !this.state.currentUser) {
-            fetch(`http://localhost:3000/profile/${this.props.currentUser.id}`, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer ' + localStorage.getItem("jwt")
-                },
-            })
-            .then(resp => resp.json())
-            .then(data => {
-                this.setState({
-                    currentUser: data,
-                    myMatches: [...data.started_matches, ...data.received_matches]
-                })
-            })
+            this.fetchCurrentUser()
         }
     }
 
+    componentDidUpdate() {
+        if(!!this.props.currentUser && !this.state.currentUser) {
+            this.fetchCurrentUser()
+        }
+    }
+
+    fetchCurrentUser = () => {
+        fetch(`http://localhost:3000/profile/${this.props.currentUser.id}`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem("jwt")
+            },
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            this.setState({
+                currentUser: data,
+                myMatches: [...data.started_matches, ...data.received_matches]
+            })
+        })
+    }
+
     renderBigLogoAndName = () => {
-        if(!this.state.currentUser) {
+        console.log(this.props.currentUser)
+        if(!this.props.currentUser) {
             return (
                 <div>
-                    <img className='home-image' src={process.env.PUBLIC_URL + '/Pug-infographic.png'}></img>
-                    <img className='home-image' src={process.env.PUBLIC_URL + '/match-fire-pngrepo-com.png'}></img>
-                    <img className='w-50' id='logo' src={process.env.PUBLIC_URL + '/petsmatch-logo.png'}></img>
+                    <img className='w-50 mt-5' id='logo' src={process.env.PUBLIC_URL + '/combined-logo.png'}></img>
                 </div>
             )
         }
